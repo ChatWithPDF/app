a5ff354413fc60dc0f6c0864642e12ff62d0b5f2
prtkjakhar <prtkjakhar@gmail.com>
2023-09-26 18:49:42 +0530

    added custom search package

--- apps/chatpdf/src/components/MiddleSide/index.tsx
+++ apps/chatpdf/src/components/MiddleSide/index.tsx
@@ -10,8 +10,8 @@ import {
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { searchPlugin } from '@react-pdf-viewer/search';
-import '@react-pdf-viewer/search/lib/styles/index.css';
+import { searchPlugin } from 'custom-pdf-search';
+// import '@react-pdf-viewer/search_/lib/styles/index.css';
 import { Spinner } from '@chakra-ui/react';
 
 const MiddleSide = () => {
@@ -25,30 +25,35 @@ const MiddleSide = () => {
   const { highlight } = searchPluginInstance;
 
   useEffect(() => {
-    function splitStringsIntoChunks(stringsArray: string[]) {
-      const result = [];
-    
-      for (let i = 0; i < stringsArray.length; i++) {
-        const words = stringsArray[i].split(' ');
-        let currentChunk: string[] = [];
-    
-        for (let j = 0; j < words.length; j++) {
-          if (currentChunk.length < 4) {
-            currentChunk.push(words[j].trim());
-          } else {
-            result.push(currentChunk.join(' '));
-            currentChunk = [words[j].trim()];
-          }
-        }
-    
-        if (currentChunk.length > 0) {
-          result.push(currentChunk.join(' '));
-        }
-      }
-    
-      return result;
-    }
-    keyword && highlight(splitStringsIntoChunks(keyword));
+    // function splitStringsIntoChunks(stringsArray: string[]) {
+    //   const result = [];
+
+    //   for (let i = 0; i < stringsArray.length; i++) {
+    //     const words = stringsArray[i].split(' ');
+    //     let currentChunk: string[] = [];
+    //     let upper_limit = 4;
+    //     if(currentChunk.length % upper_limit === 1){
+    //       upper_limit+=1;
+    //     }
+    //     for (let j = 0; j < words.length; j++) {
+    //       if (currentChunk.length < upper_limit) {
+    //         currentChunk.push(words[j].trim());
+    //       } else {
+    //         result.push(currentChunk.join(' '));
+    //         currentChunk = [words[j].trim()];
+    //       }
+    //     }
+
+    //     if (currentChunk.length > 0) {
+    //       result.push(currentChunk.join(' '));
+    //     }
+    //   }
+
+    //   return result;
+    // }
+    console.log('keyword', keyword);
+    keyword &&
+      highlight(keyword);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [keyword]);
 
@@ -59,7 +64,7 @@ const MiddleSide = () => {
           processingPdf ? (
             <div className={styles.noPdf}>
               {/* @ts-ignore */}
-              <Spinner/>
+              <Spinner />
             </div>
           ) : (
             <div
