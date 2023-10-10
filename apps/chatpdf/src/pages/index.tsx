b0ceffc864aacd53b0eb10232912ca8eaa5bb7d7
Shruti3004 <mail2shruti.ag@gmail.com>
2023-10-10 15:47:28 +0530

    fix: PDF loading
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- apps/chatpdf/src/pages/index.tsx
+++ apps/chatpdf/src/pages/index.tsx
@@ -1,15 +1,15 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import { useLocalization } from '../hooks/useLocalization';
-import dynamic from 'next/dynamic';
-import LeftSide from '../components/LeftSide';
-import MiddleSide from '../components/MiddleSide';
-import { AppContext } from '../context';
-import { useContext } from 'react';
-import NavBar from '../components/NavBar';
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useLocalization } from "../hooks/useLocalization";
+import dynamic from "next/dynamic";
+import LeftSide from "../components/LeftSide";
+import MiddleSide from "../components/MiddleSide";
+import { AppContext } from "../context";
+import { useContext } from "react";
+import NavBar from "../components/NavBar";
 
 const ChatUiWindow = dynamic(
-  () => import('../components/ChatWindow/ChatUiWindow'),
+  () => import("../components/ChatWindow/ChatUiWindow"),
   { ssr: false }
 );
 
@@ -21,45 +21,51 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title> {t('label.title')}</title>
+        <title> {t("label.title")}</title>
       </Head>
       <div
         style={{
-          width: '100vw',
-          height: '100vh',
-          display: 'flex',
-        }}>
-        {/* <div
-          style={{
-            backgroundColor: '#b99825',
-            flex: collapsed ? '0.05' : '0',
-            color: 'white',
-            padding: '1vh',
-            transition: 'all 0.2s ease',
-          }}>
-          <LeftSide />
-        </div> */}
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+        }}
+      >
         <div
           style={{
-            backgroundColor: '#0B1F3A',
+            backgroundColor: "#b99825",
+            flex: collapsed ? "0.05" : "0",
+            color: "white",
+            padding: "1vh",
+            transition: "all 0.2s ease",
+            display: "none",
+          }}
+        >
+          <LeftSide />
+        </div>
+        <div
+          style={{
+            backgroundColor: "#0B1F3A",
             flex: 1,
-          }}>
+          }}
+        >
           <MiddleSide />
         </div>
         <div
           style={{
             flex: 0.7,
-            height: '100vh'
-          }}>
+            height: "100vh",
+          }}
+        >
           <NavBar />
           <div
-            id='chatUI'
+            id="chatUI"
             style={{
-              position: 'fixed',
-              top: '90px',
-              bottom: '1vh',
-              width: '41vw',
-            }}>
+              position: "fixed",
+              top: "90px",
+              bottom: "1vh",
+              width: "41vw",
+            }}
+          >
             <ChatUiWindow />
           </div>
         </div>
@@ -68,7 +74,7 @@ const Home: NextPage = () => {
       {/* Mobile View */}
       <style jsx>{`
         @media (max-width: 767px) {
-          #chatUI{
+          #chatUI {
             width: 100% !important;
           }
           div {
